a7c138266683b49d575e045417de6653106aa167
Sarah Port <seport@users.noreply.github.com>
2021-07-28 17:14:10 -0700

    basic webpack + react

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpacks :D'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.id = "app";
 
   return element;
 }
 
 document.body.appendChild(component());
+document.body.insertAdjacentHTML("beforeend", '<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script><script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>');
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(e(LikeButton), domContainer);
