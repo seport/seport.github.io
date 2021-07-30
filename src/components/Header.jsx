ddd904584167e378f35d58e53e91c60692ff36b2
Sarah Port <seport@users.noreply.github.com>
2021-07-30 10:11:18 -0700

    scroll  nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollSpy from 'react-scrollspy-navigation';
 
 const Header = () => <div id="nav">
 <ul class="right">
@@ -7,13 +8,23 @@ const Header = () => <div id="nav">
     <li><a href="https://jsfiddle.net/user/seport/fiddles/" target="_blank">JSFiddle</a></li>
     <li><a href="https://www.linkedin.com/in/seport" target="_blank">LinkedIn</a></li>
 </ul>
-<ul>
-    <hr />
-    <li class="goabout">About</li>
-    <li class="goskills">Skills</li>
-    <li class="goprojects">Projects</li>
-    <li class="gocontact">Contact</li>
-</ul>
+<ScrollSpy>
+  <a href="#about" ref={React.createRef()}>
+    About
+  </a>
+  {` / `}
+  <a href="#skills" ref={React.createRef()}>
+    Skills
+  </a>
+  {` / `}
+  <a href="#projectswrapper" ref={React.createRef()}>
+    Projects
+  </a>
+  {` / `}
+  <a href="#contact" ref={React.createRef()}>
+    Contact
+  </a>
+</ScrollSpy>
 </div>
 
 export default Header;
