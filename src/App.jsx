d9f99e8b0736122e713759b6f391d716eb9f15c3
Sarah Port <seport@users.noreply.github.com>
2021-07-31 07:40:25 -0700

    css smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './pages/Hero';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Blogs from './pages/Blogs';
+import React, { useEffect } from "react";
+import Header from "./components/Header";
+import Hero from "./pages/Hero";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Blogs from "./pages/Blogs";
 
-const App = () => <div>
-    <Hero />
-    <Header />
-    <Skills />
-    <Blogs />
-    <Projects />
-    <Contact />
-</div>;
+const App = () => {
+//   useEffect(() => {
+//     const activeAnchor = window.location.hash;
+//     console.log(activeAnchor)
+//     if (activeAnchor) {
+//       const activeElement = document.getElementById(activeAnchor.slice(1));
+//       const scrollHeight = activeElement.offsetTop;
+//       window.scrollTo(0, scrollHeight);
+//     }
+//   });
+  return (
+    <div>
+      <Hero />
+      <Header />
+      <Skills />
+      <Blogs />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import ScrollSpy from 'react-scrollspy-navigation';
-import useIsVisible from '../hooks/useIsVisible';
-import {heroRef} from '../pages/Hero';
+import React from "react";
+// import ScrollSpy from 'react-scrollspy-navigation';
+import useIsVisible from "../hooks/useIsVisible";
+import { heroRef } from "../pages/Hero";
+
+/******  HANDLE NAV ********/
+
+const handleNav = () => {
+  
+}
+
+/************************* */
 
 const Header = () => {
-    const heroVisible = useIsVisible(heroRef);
-    
-return (<div id="nav" className={heroVisible ? "hero-visible" : ""}>
-<ul class="right">
-    <li><a href="https://twitter.com/seport_">Twitter</a></li>
-    <li><a href="https://github.com/seport" target="_blank">Github</a></li>
-    <li><a href="https://jsfiddle.net/user/seport/fiddles/" target="_blank">JSFiddle</a></li>
-    <li><a href="https://www.linkedin.com/in/seport" target="_blank">LinkedIn</a></li>
-</ul>
-<ScrollSpy duration={750}>
-  <a href="#about" ref={React.createRef()}>
-    About
-  </a>
-  {` / `}
-  <a href="#skills" ref={React.createRef()}>
-    Skills
-  </a>
-  {` / `}
-  <a href="#blogs" ref={React.createRef()}>
-    Blogs
-  </a>
-  {` / `}
-  <a href="#projectswrapper" ref={React.createRef()}>
-    Projects
-  </a>
-  {` / `}
-  <a href="#contact" ref={React.createRef()}>
-    Contact
-  </a>
-</ScrollSpy>
-</div>)}
+  const heroVisible = useIsVisible(heroRef);
+
+  return (
+    <div id="nav" className={heroVisible ? "hero-visible" : ""}>
+      <ul class="right">
+        <li>
+          <a href="https://twitter.com/seport_">Twitter</a>
+        </li>
+        <li>
+          <a href="https://github.com/seport" target="_blank">
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="https://jsfiddle.net/user/seport/fiddles/" target="_blank">
+            JSFiddle
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/seport" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+      <a href="#about">About</a>
+      {` / `}
+      <a href="#skills" onClick={handleNav}>
+        Skills
+      </a>
+      {` / `}
+      <a href="#blogs">Blogs</a>
+      {` / `}
+      <a href="#projectswrapper">Projects</a>
+      {` / `}
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
 
 export default Header;
