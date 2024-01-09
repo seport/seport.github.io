fead478f2f89d308319d814bf5b4a0df1d835110
Sarah Port <seport@Sarahs-Air.lan>
2024-01-09 09:27:57 -0800

    2021 archive

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Sarah Port',
     }),
+    new CopyWebpackPlugin({
+      patterns: [{from: './src/archive', to: '.'}]
+    })
   ],
   devtool: 'inline-source-map',
   devServer: {
