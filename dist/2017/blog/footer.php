17196013bc918c60a3ba0d4c9a7518c38c21a601
Sarah Port <seport@Sarahs-Air.lan>
2024-01-09 09:49:44 -0800

    2017 archive dist

---
+++ dist/2017/blog/footer.php
@@ -0,0 +1,15 @@
+</div>
+<div id="contact">
+	<div class="contactcontainer">
+		<h1>Want to work on something?</h1>
+		<div class="socialicons"><a href="https://github.com/seport" target="_blank"><img src="/github.png"></a>
+		<a href="https://jsfiddle.net/user/seport/fiddles/" target="_blank"><img src="/jsfiddle.png"></a>
+		<a href="https://www.linkedin.com/in/seport" target="_blank"><img src="/linkedin.png"></a></div>
+		<div class="contact"></div>
+	</div>
+	<div class="contactbottom">
+		<p>©Sarah Port 2017</p>
+	</div>
+</div>
+</body>
+</html>
